d8edd7eddd70b21e742eb4c693933c0df608a8b3
borismatovic <borismatovic@users.noreply.github.com>
2022-12-29 18:42:33 +0100

    - finished html-css part

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello from js")
+let order = [
+  {
+    name: "Pizza",
+    item: 0,
+  },
+  {
+    name: "Burger",
+    item: 0,
+  },
+  {
+    name: "Beer",
+    item: 0,
+  },
+];
+
+document.addEventListener("click", (e) => console.log(e.target.parentElement));
+
+let cartEl = document.querySelector(".cart");
+let cart = order.filter((el) => el.item > 0);
+if (cart.length) cartEl.classList.remove("hide");
+else cartEl.classList.add("hide");
